89b4f78a74df44583cf6f9a3be2d780e26bffb23
wubmush <165905533+wubmush@users.noreply.github.com>
2024-04-05 14:07:25 +0000

    Add files via upload
    Added dex entry generator

--- randmon.py
+++ randmon.py
@@ -1,7 +1,7 @@
 import random
 
 class Pokemon:
-    def __init__(self, name, type1, type2, hp, attack, defense, sp_attack, sp_defense, speed, ability, colour):
+    def __init__(self, name, type1, type2, hp, attack, defense, sp_attack, sp_defense, speed, ability, colour, dex):
         self.name = name
         self.type1 = type1
         self.type2 = type2
@@ -14,6 +14,7 @@ class Pokemon:
         self.ability = ability
         self.colour = colour
         self.base_stat_total = hp + attack + defense + sp_attack + sp_defense + speed
+        self.dex = dex
 
     def display_info(self):
         print(f"Name: {self.name}")
@@ -29,6 +30,7 @@ class Pokemon:
         print(f"SD: {self.sp_defense}")
         print(f"Sp: {self.speed}")
         print(f"BST: {self.base_stat_total}")
+        print(f"Dex: {self.dex}")
         print()
 
 def generate_pokemon():
@@ -68,6 +70,12 @@ def generate_pokemon():
 
     prefixes = ["Rapid", "Mystic", "Blizzard", "Dragon", "Frost", "Boulder", "Eclipse", "Whisper", "Venom", "Moon", "Thunder", "Tidal", "Flare", "Star", "Night", "Iron", "Shadow", "Crimson", "Leaf", "Aurora", "Steel", "Whirl", "Gale", "Mistral", "Earth", "Inferno", "Spectral", "Wraith", "Dark", "Nebula", "Lava", "Zap", "Gloom", "Lunar", "Abyssal", "Vortex", "Fluff", "Electro", "Blade", "Swift", "Solar", "Echo", "Fury", "Crystal", "Magma", "Nova", "Omega", "Radiant", "Frostbite", "Polar", "Pulse", "Apex", "Chaos", "Nova", "Vivid", "Eternal", "Sonic", "Cosmic", "Celestial", "Quasar", "Infinity", "Neon", "Supernova", "Galactic", "Phoenix", "Psycho", "Serenity", "Nebula", "Orbit", "Rogue", "Twilight", "Noble", "Velocity", "Blitz", "Obsidian", "Rebel", "Eclipse", "Quantum", "Ignite", "Shiver", "Thunderbolt", "Emerald", "Cascade", "Frostfire", "Crimson", "Harmony", "Majestic", "Infernal", "Radiance", "Vapor", "Bolt", "Sapphire", "Whiplash", "Zephyr", "Galaxy", "Dusk"]
     suffixes = ["strike", "claw", "wing", "whirl", "fang", "fire", "leaf", "sting", "howl", "crash", "wave", "blaze", "shower", "claw", "fury", "beam", "storm", "shade", "wind", "flare", "chaser", "night", "wild", "dust", "flame", "shroud", "nova", "frost", "thunder", "gale", "fury", "quasar", "void", "zenith", "spark", "blitz", "blizzard", "shock", "plasma", "flare", "breeze", "strike", "vortex", "hail", "strike", "drift", "mystique", "roar", "glow", "peak", "shiver", "quake", "cascade", "crescent", "echo", "crimson", "torrent", "freeze", "whisper", "scream", "frost", "twister", "flurry", "nova", "beam", "thunder", "flash", "quake", "frost", "bolt", "storm", "stream", "burst", "haze", "blaze", "whirlpool", "wave", "flame", "fury", "cyclone", "volt", "scorch", "blizzard", "flare", "blaze", "blast", "quiver", "scourge", "frost", "shard", "gust", "inferno", "frenzy", "strike"]
+    dex1 = ["scientists", "myths", "legends", "scientists of legend", "a vision", "scripture"]
+    dex2 = ["can", "will", "must", "likes to", "loves to", "will reluctantly", "works as we speak to", "has already completed its mission to"]
+    dex3 = ["destroy", "kill", "detonate", "explode", "hug", "breed with", "jump over", "eat", "consume", "devour", "vaporize", "liquify", "melt", "evaporate", "decimate", "annihilate", "be eaten by"]
+    dex4 = ["an Indian elephant", "a dump truck", "a skyscraper", "an ancient civilization", "an entire planet", "the Shellder on its tail", "life as we know it", "several Indian elephants", "you", "its prey", "itself"]
+    dex5 = ["fists", "kicks", "droppings", "psychic powers", "brain", "internal fire", "charm", "abdomen", "poison", "draconic energy"]
+    dex6 = ["right here and now", "in the past", "in ancient legend", "3000 years ago", "in the future", "now", "immediately", "instantly", "in seconds", "in minutes"]
 
     # Randomly select attributes
     name = f"#{random.randint(10000, 99999)} " + random.choice(prefixes) + random.choice(suffixes)
@@ -81,8 +89,9 @@ def generate_pokemon():
     speed = random.randint(10, 160)
     ability = random.choice(abilities)
     colour = random.choice(colours)
+    dex = f"According to {random.choice(dex1)}, this Pokémon {random.choice(dex2)} {random.choice(dex3)} {random.choice(dex4)} using just the power of its {random.choice(dex5)} {random.choice(dex6)}."
 
-    return Pokemon(name, type1, type2, hp, attack, defense, sp_attack, sp_defense, speed, ability, colour)
+    return Pokemon(name, type1, type2, hp, attack, defense, sp_attack, sp_defense, speed, ability, colour, dex)
 
 def generate_multiple_pokemon(num_pokemon):
     pokemons = []
